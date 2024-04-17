3f5183496ad938426dcabedae50c149c8a00b54c
J-J-Ohns <mr.j.m.lass@googlemail.com>
2024-04-17 16:25:47 +0200

    waiting for Player2

---
+++ public/game/scenes/GameScene.js
@@ -0,0 +1,9 @@
+import GeneralScene from './GeneralScene.js'
+
+class GameScene extends GeneralScene {
+    constructor(settings) {
+        super('GameScene', settings)
+    }
+}
+
+export default GameScene
